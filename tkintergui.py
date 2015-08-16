b5b063ca505d41a9615d70be93e2825c03706676
peastone <peastone@users.noreply.github.com>
2015-08-16 18:21:16 +0200

    new tkinter GUI

--- tkintergui.py
+++ tkintergui.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 This file is part of Robobackup.
 Copyright 2014, 2015 Siegfried Schoefer
@@ -15,62 +17,116 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from tkinter import filedialog, messagebox, Frame, PhotoImage, Label, \
-        Text, Button, LEFT, RIGHT, YES, END
+from tkinter import messagebox, Frame, PhotoImage, Label, \
+        Text, Button, END
 import os
+import time
+from logtools import Severity, logbook
 
 class BackupGuiTk(Frame):
-    def __init__(self, method, master=None, ask=False):
-        self.picframe = Frame.__init__(self, master, width=100, height=100)
+    """
+    The main widget of the robobackup GUI made with tkinter.
+    """
+    def __init__(self, method=None, master=None):
+        self.method = method
+        self.picframe = Frame.__init__(self, master)
+        self.master.title("Robobackup")
         self.image = PhotoImage()
         self.image["file"] = os.path.join("resources", "ASK.png")
         self.piclabel = Label(self.picframe, image=self.image)
-        self.piclabel.pack()
-        self.pack()
-        self.logframe = Frame.__init__(self, master)
-        self.label = Label(self.logframe)
-        self.label.pack(fill = "both")
-        self.loglabel = Label(self.logframe, text = "Log:")
-        self.loglabel.pack(fill = "both")
-        self.text = Text(self.logframe, width=10, height=10)
-        self.text.pack(fill = "both")
-        self.pack()
-        self.buttonframe = Frame.__init__(self, master)
-        self.save = Button(self.buttonframe, text = _("Save"), command=self.saveLog)
-        self.save.pack(side=LEFT, fill = "both", expand=YES)
-        self.close = Button(self.buttonframe, text = _("Close"), command=exit)
-        self.close.pack(side=RIGHT, fill = "both", expand=YES)
-        self.pack()
-        self.master.title("Robobackup")
-        self.startnow = True
-        if ask:
-            self.startnow = messagebox.askyesno("Robobackup",_("Start Backup now?"))
-        if (self.startnow):
-            self.image["file"] = os.path.join("resources", "BACKUP.png")
-            method(self)
-        self.mainloop()
-    def setBackupSuccess(self):
-        self.label["text"] = _("Success")
-        self.label["bg"] = "green"
-        self.image["file"] = os.path.join("resources", "SUCCESS.png")
-    def setBackupFailure(self):
-        self.label["text"] = _("Failure")
-        self.label["bg"] = "red"
-        self.image["file"] = os.path.join("resources", "FAIL.png")
-    def setBackupCheck(self):
-        self.label["text"] = _("Check backup")
-        self.label["bg"] = "orange"
-        self.image["file"] = os.path.join("resources", "CHECK.png")
-    def setLog(self, log):
-        self.text.insert(END, log)
-    def saveLog(self):
-        filedialog
-        f = filedialog.asksaveasfile(mode='w', filetypes=[(_("Text files"), "*.txt")])
-        if f is None:
-            return
-        log = str(self.text.get(1.0,END))
+        self.piclabel.grid(row=0, column=0, columnspan=4, rowspan=6)
+        self.clocklabel = Label(self.picframe, text=_("Elapsed time:"))
+        self.clocklabel.grid(row=0, column=4, sticky="NSEW")
+        self.clock = Label(self.picframe, text="")
+        self.clock.grid(row=1, column=4, sticky="NSEW")
+        self.start = Button(self.picframe, text=_("Start Backup"), command=self.__clk)
+        self.start.grid(row=3, column=4, sticky="NSEW")
+        self.errorlabel = Label(self.picframe)
+        self.errorlabel.grid(row=4, column=4, sticky="NSEW")
+        self.close = Button(self.picframe, text=_("Close"), command=self.__cls)
+        self.close.grid(row=5, column=4, sticky="NSEW")
+        self.loglabel = Label(self.picframe, text=_("Log:"), justify="left")
+        self.loglabel.grid(row=6, column=0, columnspan=5, sticky="NSEW")
+        self.text = Text(self.picframe)
+        self.text.grid(row=7, column=0, rowspan=6, columnspan=5, sticky="NSEW")
+        self.timeout = False
+        self.starttime = -1
+    def __clk(self):
+        """
+        This method starts the backup. It is a slot which is connected
+        to the signal "click" of the button "btnStart".
+        """
+        self.start.config(state="disabled")
+        self.close.config(state="disabled")
+        self.clock_tick()
         try:
-            f.write(log)
-            f.close()
+            self.method()
         except:
-            print("Error writing logfile from GUI")
+            self.update()
+            messagebox.showinfo(_("Critical"), _("Robobackup " \
+                "failed. Contact your admin."))
+        self.timeout = True
+        self.close.config(state="normal")
+    def __cls(self):
+        """
+        This method is used to close the GUI.
+        """
+        self.master.destroy()
+    def clock_tick(self):
+        """
+        This is used to count the seconds the backup method runs.
+        """
+        if not self.timeout:
+            self.starttime = int(time.time() * 1000)
+        milliseconds = int(time.time() * 1000) - self.starttime
+        seconds, milliseconds = divmod(milliseconds, 1000)
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        digitalclock = "{hh:02d}:{mm:02d}:{ss:02d}".format(hh=hours, \
+            mm=minutes, ss=seconds)
+        self.clock.configure(text=digitalclock)
+        if not self.timeout:
+            self.master.after(1000, self.clock_tick)
+    def update(self):
+        """
+        This method will be called, if logs are changing.
+        """
+        color = logbook.get_severity()
+        self.set_log(logbook.get_string())
+        if color is Severity.green:
+            self.set_backup_success()
+        elif color is Severity.orange:
+            self.set_backup_check()
+        elif color is Severity.red:
+            self.set_backup_failure()
+        else:
+            raise RuntimeError(_("Color does not match an action."))
+    def set_backup_success(self):
+        """
+        Set everything green.
+        """
+        self.errorlabel.configure(text=_("Success"))
+        self.errorlabel["bg"] = "green"
+        self.image["file"] = os.path.join(os.path.dirname(\
+            os.path.relpath(__file__)), "resources", "SUCCESS.png")
+    def set_backup_check(self):
+        """
+        Set everything orange.
+        """
+        self.errorlabel.configure(text=_("Check backup"))
+        self.errorlabel["bg"] = "orange"
+        self.image["file"] = os.path.join(os.path.dirname(\
+            os.path.relpath(__file__)), "resources", "CHECK.png")
+    def set_backup_failure(self):
+        """
+        Set everything red.
+        """
+        self.errorlabel.configure(text=_("Failure"))
+        self.errorlabel["bg"] = "red"
+        self.image["file"] = os.path.join(os.path.dirname(\
+            os.path.relpath(__file__)), "resources", "FAIL.png")
+    def set_log(self, log):
+        """
+        Write text to the GUI.
+        """
+        self.text.insert(END, log)
